89e18ff155a7b86c029549dd0478f27f20ff2948
Ignacio Brevis <88429528+ibrevis@users.noreply.github.com>
2024-07-14 20:17:33 +0100

    Server examples

---
+++ GPU_run.py
@@ -0,0 +1,19 @@
+import torch
+import numpy as np
+from time import sleep, time
+
+device = 'cuda'
+
+A = torch.rand((20000,20000),device=device)
+B = torch.rand((20000,20000),device=device)
+
+t0 = time()
+for i in range(20):
+    torch.matmul(A,B)
+    print(i)
+    sleep(2)
+print((time() - t0)/60,' [min]')  
+
+
+
+
